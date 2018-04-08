5d7e63a9b451735cb2d2eccb54d3a159b72b9c38
students <pamazaev@almaren.yandex.ru>
2018-04-08 20:15:11 +0300

    Classifier does help

--- GAN/run_muse_validation.sh
+++ GAN/run_muse_validation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES="7"
+export CUDA_VISIBLE_DEVICES="5"
 
 cd ../../MUSE
 python evaluate.py --src_lang fr --tgt_lang en --src_emb ../gan_embeddings/GAN/embeds_1_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_2_tmp.vec >> ../gan_embeddings/validation/tgt_src.txt 2>&1
