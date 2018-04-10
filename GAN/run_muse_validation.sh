79ff2949e414a83dd6766df400ded04230c222c8
students <pamazaev@almaren.yandex.ru>
2018-04-10 10:32:03 +0300

    Tried en-fr

--- GAN/run_muse_validation.sh
+++ GAN/run_muse_validation.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES="5"
 
 cd ../../MUSE
-python evaluate.py --src_lang fr --tgt_lang en --src_emb ../gan_embeddings/GAN/embeds_1_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_2_tmp.vec >> ../gan_embeddings/validation/tgt_src.txt 2>&1
-python evaluate.py --src_lang en --tgt_lang fr --src_emb ../gan_embeddings/GAN/embeds_2_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_1_tmp.vec >> ../gan_embeddings/validation/src_tgt.txt 2>&1
+python evaluate.py --src_lang en --tgt_lang fr --src_emb ../gan_embeddings/GAN/embeds_1_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_2_tmp.vec >> ../gan_embeddings/validation/tgt_src.txt 2>&1
+python evaluate.py --src_lang fr --tgt_lang en --src_emb ../gan_embeddings/GAN/embeds_2_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_1_tmp.vec >> ../gan_embeddings/validation/src_tgt.txt 2>&1
