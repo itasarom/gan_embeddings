26691bc0c1435f4bc26673345a7c3574b380e61f
students <pamazaev@almaren.yandex.ru>
2018-04-22 18:03:00 +0300

    Going to use MUSE validation in my code

--- refactoring/run_muse_validation.sh
+++ refactoring/run_muse_validation.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES="5"
 
 cd ../../MUSE
-python evaluate.py --src_lang en --tgt_lang es --src_emb ../gan_embeddings/GAN/embeds_1_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_2_tmp.vec >> ../gan_embeddings/validation/tgt_src.txt 2>&1
-python evaluate.py --src_lang es --tgt_lang en --src_emb ../gan_embeddings/GAN/embeds_2_tmp.vec --tgt_emb ../gan_embeddings/GAN/embeds_1_tmp.vec >> ../gan_embeddings/validation/src_tgt.txt 2>&1
+python evaluate.py --src_lang en --tgt_lang es --src_emb ../gan_embeddings/refactoring/embeds_1_tmp.vec --tgt_emb ../gan_embeddings/refactoring/embeds_2_tmp.vec >> ../gan_embeddings/validation/tgt_src.txt 2>&1
+python evaluate.py --src_lang es --tgt_lang en --src_emb ../gan_embeddings/refactoring/embeds_2_tmp.vec --tgt_emb ../gan_embeddings/refactoring/embeds_1_tmp.vec >> ../gan_embeddings/validation/src_tgt.txt 2>&1
